783439d3e294fabb1cb6f996c55fbe58ecdc3416
利开团 <likaituan@wecash.net>
2017-05-27 18:08:01 +0800

    quick submit

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
  * Created by likaituan on 25/05/2017.
  */
 
-var onPageInit = function () {
+window.onload = function () {
+	var isLoaded = {};
 	document.body.innerHTML = `
 	<table border="1">
 		<tr>
@@ -10,15 +11,19 @@ var onPageInit = function () {
 			<th>Cate</th>
 			<th>Link</th>
 		</tr>
-		${Data.map(item=>{return `
-		<tr>
-			<td><a href="${item.link}" target="_blank">${item.name}</a></td>
-			<td>${item.cate}</td>
-			<td>${item.link}</td>
-		</tr>
+		${Data.map(item => {
+			var itemHTML = '';
+			if (!isLoaded[item.cate]) {
+				isLoaded[item.cate] = true;
+				itemHTML = `<tr><td colspan="3">${item.cate}</td></tr>`;
+			}
+			return `${itemHTML}
+			<tr>
+				<td><a href="${item.link}" target="_blank">${item.name}</a></td>
+				<td>${item.scat}</td>
+				<td>${item.link}</td>
+			</tr>
 		`}).join('')}
 	</table>
 	`;
 };
-
-window.onload = onPageInit;
--- modules/data.js
+++ modules/data.js
@@ -6,85 +6,223 @@ var Data = [
 	// 编辑器
 	{
 		cate: 'editor',
-		name: 'Xcode',
-		link: 'https://developer.apple.com/xcode/'
-	},
-	{
-		cate: 'editor',
+		scat: 'jetbrains',
 		name: 'WebStorm',
 		link: 'https://www.jetbrains.com/webstorm/'
 	},
-
-	// 框架
 	{
-		cate: 'framework',
-		name: 'gradle',
-		link: 'https://gradle.org/'
-	},
-	{
-		cate: 'framework',
-		name: 'vue',
-		link: 'https://vuejs.org/'
-	},
-	{
-		cate: 'framework',
-		name: 'angular',
-		link: 'https://angularjs.org/'
-	},
-	{
-		cate: 'framework',
-		name: 'react',
-		link: 'https://facebook.github.io/react/'
-	},
-	{
-		cate: 'framework',
-		name: 'react-native',
-		link: 'https://facebook.github.io/react-native/'
-	},
-	{
-		cate: 'framework',
-		name: 'weex',
-		link: 'https://weex.apache.org/'
+		cate: 'editor',
+		scat: 'apple',
+		name: 'Xcode',
+		link: 'https://developer.apple.com/xcode/'
+	}, {
+		cate: 'editor',
+		scat: 'google',
+		name: 'Android Studio',
+		link: 'https://developer.android.com/studio/index.html'
 	},
 
 	//语言
 	{
 		cate: 'language',
-		name: 'swift',
-		link: 'https://developer.apple.com/swift/'
-	},
-	{
-		cate: 'language',
+		scat: 'js',
 		name: 'es6',
 		link: 'http://es6.ruanyifeng.com/'
 	},
 	{
 		cate: 'language',
+		scat: 'js',
+		name: 'node',
+		link: 'https://nodejs.org'
+	},
+	{
+		cate: 'language',
+		scat: 'app',
+		name: 'swift',
+		link: 'https://developer.apple.com/swift/'
+	},
+	{
+		cate: 'language',
+		scat: 'app',
 		name: 'objective-c',
 		link: 'https://developer.apple.com/reference/objectivec'
 	},
 	{
 		cate: 'language',
+		scat: 'app',
 		name: 'groovy',
 		link: 'http://groovy-lang.org/'
+	},
+
+	// 框架
+	{
+		cate: 'framework',
+		scat: 'node',
+		name: 'express',
+		link: 'http://expressjs.com/'
+	},
+	{
+		cate: 'framework',
+		scat: 'js',
+		name: 'vue',
+		link: 'https://vuejs.org/'
+	},
+	{
+		cate: 'framework',
+		scat: 'js',
+		name: 'angular',
+		link: 'https://angularjs.org/'
+	},
+	{
+		cate: 'framework',
+		scat: 'js',
+		name: 'react',
+		link: 'https://facebook.github.io/react/'
+	},
+	{
+		cate: 'framework',
+		scat: 'app',
+		name: 'react-native',
+		link: 'https://facebook.github.io/react-native/'
+	},
+	{
+		cate: 'framework',
+		scat: 'app',
+		name: 'weex',
+		link: 'https://weex.apache.org/'
+	},
+	{
+		cate: 'framework',
+		scat: 'app',
+		name: 'cordova',
+		link: 'https://cordova.apache.org/'
+	},
+	{
+		cate: 'framework',
+		scat: 'app',
+		name: 'gradle',
+		link: 'https://gradle.org/'
+	},
+	{
+		cate: 'framework',
+		scat: 'app',
+		name: 'ionic',
+		link: 'https://ionicframework.com/'
+	},
+	{
+		cate: 'framework',
+		scat: 'css',
+		name: 'bootstrap',
+		link: 'http://getbootstrap.com/'
+	},
+	{
+		cate: 'framework',
+		scat: 'css',
+		name: 'sass',
+		link: 'http://sass-lang.com/'
+	},
+	{
+		cate: 'framework',
+		scat: 'css',
+		name: 'less',
+		link: 'http://lesscss.org/'
 	},
 
 	// 工具
 	{
 		cate: 'tool',
+		scat: 'build',
 		name: 'webpack',
 		link: 'https://webpack.github.io/'
 	},
 	{
 		cate: 'tool',
+		scat: 'build',
+		name: 'babel',
+		link: 'https://webpack.github.io/'
+	},
+	{
+		cate: 'tool',
+		scat: 'validate',
+		name: 'eslint',
+		link: 'http://eslint.org/'
+	},
+	{
+		cate: 'tool',
+		scat: 'code',
 		name: 'code-push',
-		link: 'http://microsoft.github.io/code-push/'
+		link: 'https://babeljs.io/'
+	},
+	{
+		cate: 'tool',
+		scat: 'code',
+		name: 'git',
+		link: 'https://git-scm.com/'
+	},
+	{
+		cate: 'tool',
+		scat: 'node',
+		name: 'npm',
+		link: 'https://www.npmjs.com/'
+	},
+
+	// 服务器
+	{
+		cate: 'server',
+		scat: 'server',
+		name: 'nginx',
+		link: 'https://nginx.org/'
+	},
+	{
+		cate: 'server',
+		scat: 'database',
+		name: 'mongodb',
+		link: 'https://docs.mongodb.com/'
+	},
+	{
+		cate: 'server',
+		scat: 'cache',
+		name: 'redis',
+		link: 'https://redis.io/'
 	},
 
 	// 插件
 	{
 		cate: 'plugin',
+		scat: 'database',
+		name: 'mongodb',
+		link: 'http://mongodb.github.io/node-mongodb-native/'
+	},
+	{
+		cate: 'plugin',
+		scat: 'cache',
+		name: 'ioredis',
+		link: 'https://github.com/luin/ioredis'
+	},
+	{
+		cate: 'plugin',
+		scat: 'web',
+		name: 'socket.io',
+		link: 'https://socket.io/'
+	},
+	{
+		cate: 'plugin',
+		scat: 'web',
 		name: 'Moment.js',
 		link: 'http://momentjs.cn/'
+	},
+
+	// 综合
+	{
+		cate: 'tutorial',
+		scat: 'w3c',
+		name: 'w3schools',
+		link: 'https://www.w3schools.com/'
+	},
+	{
+		cate: 'tutorial',
+		scat: 'FrontEnd',
+		name: '妙味课堂',
+		link: 'http://www.miaov.com/'
 	}
 ];
